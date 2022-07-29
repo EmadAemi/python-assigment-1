fb82f6d571f7a9e88ebd83ab77c4736dc23f7c08
Emad Aemi <66124566+EmadAemi@users.noreply.github.com>
2022-07-29 22:39:16 +0430

    Delete 8 - Triangle Possibility.py

--- 8 - Triangle Possibility.py
+++
@@ -1,7 +0,0 @@
-a = int(input("First Side: "))
-b = int(input("Second Side: "))
-c = int(input("Third Side: "))
-if a**2<=b**2+c**2 and b**2<=a**2+c**2 and c**2<=a**2+b**2:
-    print("Triangle")
-else:
-    print("Not Triangle")
